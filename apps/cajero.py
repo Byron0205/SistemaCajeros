a6d388ed1b9e59bcc4ecdb8231e18ca1047150d2
FranciscoCastill <fcastillo075@gmail.com>
2023-06-21 00:44:58 -0600

    Prediseño de los cajeros

--- apps/cajero.py
+++ apps/cajero.py
@@ -1,6 +1,7 @@
 from tkinter import ttk
 from tkinter import Toplevel
 from tkinter import StringVar
+import tkinter as tk
 
 class Cajero(Toplevel):
     def __init__(self, root):
@@ -13,11 +14,30 @@ class Cajero(Toplevel):
     
     def create_widgets(self):
         self.pantalla.set('pantalla cajero')
-        lbl_title = ttk.Label(self, textvariable=self.pantalla, background='blue')
-        lbl_title.pack()
-
-        btnNumero1= ttk.Button(self, text='1',command=lambda:self.pantalla.set('selecciono 1'))
-        btnNumero1.pack()
         
-        btnNumero1= ttk.Button(self, text='2',command=lambda:self.pantalla.set('selecciono 2'))
-        btnNumero1.pack()
+        frame_principal = ttk.Frame(self)
+        frame_principal.pack(pady=20, fill=tk.BOTH, expand=True)
+
+        frame_izquierdo = ttk.Frame(frame_principal)
+        frame_izquierdo.pack(side=tk.LEFT, padx=10)
+
+        frame_derecho = ttk.Frame(frame_principal)
+        frame_derecho.pack(side=tk.RIGHT, padx=10)
+
+        lbl_pantalla = ttk.Label(frame_principal, textvariable=self.pantalla, background='blue')
+        lbl_pantalla.pack()
+        # Botones de la lado izquierdo con los numeros
+        btn_numero1 = ttk.Button(frame_izquierdo, text='50,000.00', command=lambda: self.pantalla.set('Seleccionaste 1'))
+        btn_numero1.pack(pady=5)
+        btn_numero2 = ttk.Button(frame_izquierdo, text='30,000.00', command=lambda: self.pantalla.set('Seleccionaste 1'))
+        btn_numero2.pack(pady=5)
+        btn_numero3 = ttk.Button(frame_izquierdo, text='10,000.00', command=lambda: self.pantalla.set('Seleccionaste 1'))
+        btn_numero3.pack(pady=5)
+        # Botones de la lado derecho con los numeros
+        btn_numero4 = ttk.Button(frame_derecho, text='Retiros', command=lambda: self.pantalla.set('Seleccionaste 2'))
+        btn_numero4.pack(pady=5)
+        btn_numero5 = ttk.Button(frame_derecho, text='Consultar', command=lambda: self.pantalla.set('Seleccionaste 2'))
+        btn_numero5.pack(pady=5)
+        btn_numero6 = ttk.Button(frame_derecho, text='Depositos', command=lambda: self.pantalla.set('Seleccionaste 2'))
+        btn_numero6.pack(pady=5)
+
