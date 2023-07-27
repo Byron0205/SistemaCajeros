fe5a480bd074345c4b551b4c31ca71bfa76a3505
Byron0205 <gabossosa@gmail.com>
2023-07-26 18:19:32 -0600

    implementacion de retiros en el sistema

--- apps/cajero.py
+++ apps/cajero.py
@@ -191,15 +191,18 @@ class Cajero(Toplevel):
                 self.pantalla.set(mensaje)
                 pass
         if(self.Tipo_Transaccion == "Retiro"):
-            if(saldo_cajero>='50000' ):
-                if(saldo_Usuario>='50000'):
-                    #Si se puede realizar el retiro
+            if(saldo_cajero>=50000 ):
+                if(saldo_Usuario>=50000):
+                    retirar_saldoCliente(self.monto.get(),self.idUsuario)
+                    retirar_saldoTarjetaCliente(self.monto.get(),self.idUsuario,self.tarjeta)
+                    modificar_saldoCajero(self.monto.get(), self.number)
+                    registro_Movimiento(self.idUsuario,self.monto.get(),self.number,2)
                     pass
                 else:
-                    mensaje = "Su cuenta bancaria no cuenta con los suficientes montos"
+                    mensaje = "Su cuenta bancaria no cuenta con los suficientes fondos"
                     self.pantalla.set(mensaje)
             else:
-                mensaje = "El cajero no cuenta con los suficientes montos"
+                mensaje = "El cajero no cuenta con los suficientes fondos"
                 self.pantalla.set(mensaje)
         #iniciar el proceso de comprobacion de fondos
         self.after(3000, self.evento1.set)
@@ -227,10 +230,12 @@ class Cajero(Toplevel):
                 self.pantalla.set(mensaje)
                 pass
         if(self.Tipo_Transaccion == "Retiro"):
-            if(saldo_cajero>='50000' ):
-                if(saldo_Usuario>='50000'):
-                    #Si se puede realizar el retiro
-                    pass
+            if(saldo_cajero>=30000 ):
+                if(saldo_Usuario>=30000):
+                    retirar_saldoCliente(self.monto.get(),self.idUsuario)
+                    retirar_saldoTarjetaCliente(self.monto.get(),self.idUsuario,self.tarjeta)
+                    modificar_saldoCajero(self.monto.get(), self.number)
+                    registro_Movimiento(self.idUsuario,self.monto.get(),self.number,2)
                 else:
                     mensaje = "Su cuenta bancaria no cuenta con los suficientes montos"
                     self.pantalla.set(mensaje)
@@ -263,10 +268,12 @@ class Cajero(Toplevel):
                 self.pantalla.set(mensaje)
                 pass
         if(self.Tipo_Transaccion == "Retiro"):
-            if(saldo_cajero>='50000' ):
-                if(saldo_Usuario>='50000'):
-                    #Si se puede realizar el retiro
-                    pass
+            if(saldo_cajero>=10000 ):
+                if(saldo_Usuario>=10000):
+                    retirar_saldoCliente(self.monto.get(),self.idUsuario)
+                    retirar_saldoTarjetaCliente(self.monto.get(),self.idUsuario,self.tarjeta)
+                    modificar_saldoCajero(self.monto.get(), self.number)
+                    registro_Movimiento(self.idUsuario,self.monto.get(),self.number,2)
                 else:
                     mensaje = "Su cuenta bancaria no cuenta con los suficientes montos"
                     self.pantalla.set(mensaje)
--- baseDatos/funcionescajero.py
+++ baseDatos/funcionescajero.py
@@ -151,6 +151,20 @@ def insertar_saldoTarjetaCliente(monto,IDusuario, num_tarjeta):
     conexion.commit()
     conexion.close()
 
+#metodo para realizar un retiro de la tarjeta del cliente
+def retirar_saldoTarjetaCliente(monto,IDusuario, num_tarjeta):
+    saldo= Ver_Saldo_Tajerta(IDusuario,num_tarjeta)
+    FondosCuenta = saldo[0] - int(monto)
+    consulta = "UPDATE UsuarioxTarjeta set monto  = ? WHERE IDusuario = ? and numero_tarjeta = ?"
+    parametros= (FondosCuenta,IDusuario,num_tarjeta)
+    conexion = establecer_conexion()
+    cursor=conexion.cursor() 
+    cursor.execute(consulta,parametros)
+    conexion.commit()
+    conexion.close()
+
+
+
 #metodo para guardar el saldo total del cliente
 def insertar_saldoCliente(monto,IDusuario):
     saldo= Ver_Saldo_Usuario(IDusuario)
@@ -163,27 +177,46 @@ def insertar_saldoCliente(monto,IDusuario):
     conexion.commit()
     conexion.close()
 
-def insertar_saldoCajero(monto,IDcajero):
-    consulta = "UPDATE Cajeros set monto  = ? WHERE IDcajero = ?"
-    parametros= (monto,IDcajero,)
+#metodo para realizar retiros al saldo del cliente
+def retirar_saldoCliente(monto,IDusuario):
+    saldo= Ver_Saldo_Usuario(IDusuario)
+    FondosCuenta =  saldo[0]-int(monto)
+    consulta = "UPDATE Usuarios set saldo  = ? WHERE IDusuario = ?"
+    parametros= (FondosCuenta,IDusuario,)
     conexion = establecer_conexion()
     cursor=conexion.cursor() 
     cursor.execute(consulta,parametros)
     conexion.commit()
     conexion.close()
 
-def realizarRetiro(Monto, idUsuario,idCajero):
-    conexion = establecer_conexion()
-    #falta metodo para obtener saldos cajero y cuenta usuario
 
-    SaldoRestanteUsuario = Monto
-    SaldoRestanteCajero = Monto
-    consultaUsuario = f'UPDATE Usuarios set saldo = ? where IDusuario=?'
-    consultaCajero = f'UPDATE Cajeros set saldo = ? where IDcajero=?'
-    ParametrosUsuario = (SaldoRestanteUsuario, idUsuario)
-    ParametrosCajero = (SaldoRestanteCajero, idCajero)
-    cursor= conexion.cursor()
-    cursor.execute(consultaUsuario, ParametrosUsuario)
-    cursor.execute(consultaCajero, ParametrosCajero)
+def Ver_Saldo_cajero(IDcajero):
+    consulta = "SELECT monto FROM cajeros WHERE IDcajero = ?"
+    Parametros = (IDcajero,)
+    conexion = establecer_conexion()
+    if conexion is not None:
+        cursor=conexion.cursor()    
+        cursor.execute(consulta,Parametros)
+        resultados = cursor.fetchall()
+        conexion.close()
+        # Verificar si hay resultados
+        if resultados:
+            # Obtener solo la lista de resultados
+            saldo_usuario = resultados[0]
+            return saldo_usuario
+        else:
+            return []  # Devolver una lista vacía si no hay resultados
+    else:
+        print('error al establecer la conexion')
+
+def modificar_saldoCajero(monto,IDcajero):
+    saldo_Cajero = Ver_Saldo_cajero(IDcajero)
+    Monto_cajero = int(saldo_Cajero[0]) - int(monto)
+    consulta = "UPDATE Cajeros set monto  = ? WHERE IDcajero = ?"
+    parametros= (Monto_cajero,IDcajero,)
+    conexion = establecer_conexion()
+    cursor=conexion.cursor() 
+    cursor.execute(consulta,parametros)
     conexion.commit()
     conexion.close()
+
